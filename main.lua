98fc4d3256e85b72b819452db37c269d7ef0c129
obi3112 <drazeobi3112@gmail.com>
2023-06-29 15:11:27 -0500

    lua file to write the main.tic contents to main.txt instead of writing it manually

---
+++ main.lua
@@ -0,0 +1,10 @@
+--[[
+this is the lua file i coded to read all contents from the main.tic file
+then write it to the main.txt file instead of manually writing it everytime
+
+]]
+
+file = io.input("main.tic")
+main = io.input("main.txt")
+main:write(file:read)
+file:close()
